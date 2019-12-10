19e85573a4f95eced54496f2af7455733e9739bc
Audie <audie.menut@gmail.com>
2019-12-10 14:21:55 +0100

    Step 3

--- app/index.js
+++ app/index.js
@@ -8,16 +8,20 @@ app.get("/", function(req, res) {
 
 app.get("/beers", function(req, res) {
   console.log("Received request for beers from", req.ip);
-  res.send("Hello beers");
+  res.json(beerList);
 });
 
 app.get("/beer/:beerId", function(req, res) {
   console.log("Received request for " + req.params["beerId"] + " from", req.ip);
-  res.send("Hello beer " + req.params["beerId"]);
+  var beerDetails = require("./beers/" + req.param("beerId") + ".json");
+  res.json(beerDetails);
 });
 
 app.use("/img", express.static("img"));
 app.use(express.static("public"));
+
+var beerList = require("./beers/beers.json");
+console.log("Beers", beerList);
 
 var server = app.listen(3000, function() {
   var host = "localhost" || server.address().address;
